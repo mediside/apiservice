b45c15cefe503d7f422de876d766a95fcbb0ff01
Maksim <lopatinma@mts.ru>
2025-09-27 17:26:13 +0300

    feat(dicom): test dicom parse lib

---
+++ cmd/dicomreader/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"log"
+
+	"github.com/suyashkumar/dicom"
+	"github.com/suyashkumar/dicom/pkg/tag"
+)
+
+func main() {
+	filepath := "/home/m/dev/hackathon/apiservice/researches/45e15946-9389-4919-8c4c-a8cc7fd46a08/norma_anon.zip"
+	r, err := zip.OpenReader(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		if !f.FileInfo().IsDir() {
+			bytesToRead := f.FileInfo().Size()
+			rc, err := f.Open()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			// Парсим DICOM из потока в памяти
+			d, err := dicom.Parse(rc, bytesToRead, nil)
+			rc.Close()
+			if err != nil {
+				log.Println("Error parsing DICOM:", err)
+				continue
+			}
+
+			fmt.Println("Parsed DICOM file:", f.Name)
+
+			patientNameElem, err := d.FindElementByTag(tag.Manufacturer)
+			if err != nil {
+				fmt.Println("tag not found:", err)
+			} else {
+				fmt.Println("tag:", patientNameElem.Value)
+			}
+		}
+
+	}
+}
